7fadb025451a526d208eea77b8eac6ae3f619e50
DuongQuangQuy <116633470+DuongQuangQuy@users.noreply.github.com>
2026-06-08 13:07:19 +0700

    [IMP] tiny_sale_purchase

--- custom_addons/tiny_sale_purchase/models/sale.py
+++ custom_addons/tiny_sale_purchase/models/sale.py
@@ -13,3 +13,21 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     purchase_multi_origin = fields.Char(string='Origin purchase company service')
+
+    def _action_confirm(self):
+        result = super(SaleOrder, self)._action_confirm()
+        if self.company_id.is_service:
+            purchase_orders = self._get_purchase_orders()
+            for order in purchase_orders:
+                order.button_confirm()
+
+        return result
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    # def _purchase_service_create(self, quantity=False):
+    #     res = super()._purchase_service_create(quantity=quantity)
+    #     print("res", res)
+    #     return res
+        
